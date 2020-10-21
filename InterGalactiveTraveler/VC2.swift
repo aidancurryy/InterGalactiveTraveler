1b16f7552ae9058b113e1d74d154ed1b491e270e
aidancurryy <aidancurryy@gmail.com>
2020-10-21 09:38:20 -0400

    Day 1

---
+++ InterGalactiveTraveler/VC2.swift
@@ -0,0 +1,30 @@
+//
+//  VC2.swift
+//  InterGalactiveTraveler
+//
+//  Created by  on 10/21/20.
+//  Copyright © 2020 ChemsApps. All rights reserved.
+//
+
+import UIKit
+
+class VC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
